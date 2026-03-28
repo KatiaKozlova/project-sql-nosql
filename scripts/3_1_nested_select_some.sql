8bb54bda6dbe39f9ce64068b4c3bc02db6863d26
Kate <katia.kozlova1@gmail.com>
2026-03-28 13:18:50 +0300

    bugfix

--- scripts/3_1_nested_select_some.sql
+++ scripts/3_1_nested_select_some.sql
@@ -1,6 +1,6 @@
 -- Nested SELECT:
 -- Show name, artist and popularity of the speechful songs (>0.8).
-SELECT track_name, track_artist, track_popularity FROM project.tracks AS t
+SELECT track_name, track_artist, track_popularity FROM tracks AS t
 WHERE t.track_id IN 
 	(SELECT track_id FROM track_characteristics 
 	WHERE speechiness > 0.8);
